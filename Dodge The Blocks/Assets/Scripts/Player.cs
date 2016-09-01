c9963dcc9f3f05895d07580a86b6812c4d1ceea6
Marloxo <marloo-_-@hotmail.com>
2016-09-01 17:09:38 +0300

    :tada: Initial Game

---
+++ Dodge The Blocks/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+	public float mapWidth =5f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        //work cross-platform
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        rb.MovePosition(rb.position + Vector2.right * x);
+    }
+}
