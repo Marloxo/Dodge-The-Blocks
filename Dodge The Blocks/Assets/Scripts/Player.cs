986607634fca97020384555dd9915208f1a8cfaa
Marloxo <marloo-_-@hotmail.com>
2016-09-01 23:13:12 +0300

    :sparkles: Implement basic Block spawner

--- Dodge The Blocks/Assets/Scripts/Player.cs
+++ Dodge The Blocks/Assets/Scripts/Player.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
     public float speed = 15f;
-	public float mapWidth =5f;
+    public float mapWidth = 5f;
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+    ///
+    //used for physic primary because it's update on fixed time  
+    ///
     void FixedUpdate()
     {
         //work cross-platform
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
 
-        rb.MovePosition(rb.position + Vector2.right * x);
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+        rb.MovePosition(newPosition);
     }
 }
